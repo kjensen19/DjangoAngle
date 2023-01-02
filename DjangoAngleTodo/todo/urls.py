080d1d91d7248832e6dc73c803dd2ddf94c1ef08
Kyle Jensen <kjensen19@gmail.com>
2023-01-01 21:43:51 -0600

    wire in detail and index placeholder pages

--- DjangoAngleTodo/todo/urls.py
+++ DjangoAngleTodo/todo/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'todo'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:task_id>/', views.detail, name='detail')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail')
 ]
 
 
--- DjangoAngleTodo/todo/views.py
+++ DjangoAngleTodo/todo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404 ,render
+from django.views import generic
+from .models import Task
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'todo/index.html'
+    
+    def get_queryset(self):
+        """
+        Return total ToDo list
+        """
+        return Task.objects.order_by('complete')
+
+
+
+class DetailView(generic.DetailView):
+    model = Task
+    template_name = 'todo/detail.html'
