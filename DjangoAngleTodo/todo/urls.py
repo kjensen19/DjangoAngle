cefa75197edc1e5eb94d61cdef70243d35ac8923
Kyle Jensen <kjensen19@gmail.com>
2023-01-03 14:19:22 -0600

    functioning fullstack, still needs work detail view link is not active

--- DjangoAngleTodo/todo/urls.py
+++ DjangoAngleTodo/todo/urls.py
@@ -17,7 +17,7 @@ from django.urls import re_path
 from todo import views 
  
 urlpatterns = [ 
-    re_path(r'^todo$', views.todo_list),
-    re_path(r'^todo/(?P<pk>[0-9]+)$', views.todo_detail),
-    re_path(r'^todo/complete$', views.todo_list_complete)
+    re_path(r'^api/todo$', views.todo_list),
+    re_path(r'^api/todo/(?P<pk>[0-9]+)$', views.todo_detail),
+    re_path(r'^api/todo/complete$', views.todo_list_complete)
 ]
